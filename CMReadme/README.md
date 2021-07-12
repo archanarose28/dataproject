040b0db0d12f19046fbbb2b2cf930016dcf7b42d
ARCHANA ROSE BIJU <archanrose@gmail.com>
2021-07-13 01:53:44 +0530

    first commit

---
+++ CMReadme/README.md
@@ -0,0 +1,62 @@
+# **Data Project** 
+
+<details open>
+<summary> Table of Contents </summary>
+
++ 1. About The Project
++ 2. Built With
++ 3. Getting Started
+    + Prerequisites
+    + Installation
++ Usage
++ Acknowledgements
+</details>
+
+1. ### **About The Project**
+
+    The project is to read a CSV file, do some aggregation/filtering/grouping functions and print out the results in table format using csharp. source data is the company registration data for any state, from data.gov.in, the Government of India's open data platform.
+
+2. ### **Built With**
+
+ * Csharp
+
+    C# is a general-purpose, multi-paradigm programming language encompassing static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines.
+
+
+3. ### **Getting Started**
+
+   #### **Prerequisites**
+ * Visual studio-
+
+    Visual Studio 2019 with the .NET Desktop Development workload.
+    
+   #### **Installation**
+   1. Installation of NuGet packages-
+   NuGet packages can be installed into any .NET project, provided that the package supports the same target framework as the project.
+   2. In Solution Explorer, right-click either References or a project and select Manage NuGet Packages....
+   3. The Browse tab displays packages by popularity from the currently selected source (see package sources). Search for a specific package using the search box on the upper left. 
+   4. Select a package from the list to display its information, which also enables the Install button along with a version-selection drop-down. For this project, CsvHelper and ConsoleTables were used.
+   <br>
+
+        *CsvHelper - Helps parsing your CSV files.* 
+    <br>
+
+       *ConsoleTables - Helps to print good-looking tables on the console.*
+    <br>
+
+   5. Select the desired version from the drop-down and select Install. 
+
+
+
+4. ### **Usage**
+   The project is usable in situations when we need to filter or retrieve data from a csv file. 
+
+5. ### **Acknowledgements**
+
+
+
+    [CsvHelper](https://joshclose.github.io/CsvHelper/getting-started/)
+
+    [Console Tables](https://www.nuget.org/packages/ConsoleTables/)
+
+    [C# Corner](https://www.c-sharpcorner.com/article/how-to-read-data-from-csv-file-in-c-sharp/)
---
+++ Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using ConsoleTables;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using System.Collections.Generic;
+
+
+namespace CompanyMaster
+{
+    public class CompanyMas
+    {
+        public string AUTHORIZED_CAP { get; set; }
+        public string DATE_OF_REGISTRATION { get; set; }
+
+        public string PRINCIPAL_BUSINESS_ACTIVITY_AS_PER_CIN { get; set; }
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int val1 = 0, val2 = 0, val3 = 0, val4 = 0, val5 = 0;  //TESTCASE1
+            using (var reader = new StreamReader("C:\\Users\\ARCHANA ROSE BIJU\\Downloads\\Tripura.csv")) //TESTCASE1
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    csv.Read();
+                    csv.ReadHeader();
+                    while (csv.Read())
+                    {
+                        var record = csv.GetRecord<CompanyMas>();
+                        double k = Convert.ToDouble(record.AUTHORIZED_CAP);
+                        if (k <= 100000)
+                            val1 += 1;
+                        else if (k > 100000 && k <= 1000000)
+                            val2 += 1;
+                        else if (k > 1000000 && k <= 10000000)
+                            val3 += 1;
+                        else if (k > 10000000 && k <= 100000000)
+                            val4 += 1;
+                        else
+                            val5 += 1;
+
+                    }   
+                }
+
+            }
+            var table = new ConsoleTable("Bin", "Counts");
+            table.AddRow("<= 1L", val1).AddRow(" 1L to 10L", val2).AddRow("10L to 1Cr ", val3)
+                .AddRow("1Cr to 10Cr ", val4).AddRow(">10Cr ", val5);
+            table.Write();
+
+
+            int[] arrCount = new int[20];     //TESTCASE2
+            int[] arrayYear = new int[20];
+            int count;
+            int Result;
+            for (int i = 0, j = 2000; i < 20 && j <= 2019; i++, j++)
+            {
+                arrayYear[i] = j;
+                arrCount[i] = 0;
+            }
+
+            //testcase2- 2000-2019 YEAR IN WHOLE LIST 
+
+            for (int k = 0; k <= 19; k++)
+            {
+                count = 0;
+                Result = arrayYear[k];
+              
+
+                using (var reader = new StreamReader("C:\\Users\\ARCHANA ROSE BIJU\\Downloads\\Tripura.csv"))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    csv.Read();
+                    csv.ReadHeader();
+                    while (csv.Read())
+                    {
+                        var record = csv.GetRecord<CompanyMas>();
+
+                        string dateStr = record.DATE_OF_REGISTRATION;
+                        DateTime checkdate = Convert.ToDateTime(dateStr);
+                        int eachyear = checkdate.Year;
+
+                        if (eachyear == Result)
+                            count += 1;
+                    }
+
+                }
+                arrCount[k] = count;
+        } //for
+            table = new ConsoleTable("Year", "Count");
+
+            for (int j = 0; j < 20; j++)
+            {
+                table.AddRow(arrayYear[j], arrCount[j]);
+            }
+            table.Write();
+
+            //TESTCASE 3
+            Dictionary<string, int> PRINCIPLE = new Dictionary<string, int>();
+            using (var reader = new StreamReader("C:\\Users\\ARCHANA ROSE BIJU\\Downloads\\Tripura.csv"))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+
+                    var rows = csv.GetRecords<CompanyMas>();
+                    foreach (var row in rows)
+                    {
+
+                        string pba = row.PRINCIPAL_BUSINESS_ACTIVITY_AS_PER_CIN;
+                        string dateStrr = row.DATE_OF_REGISTRATION;
+                        DateTime checkdatee = Convert.ToDateTime(dateStrr);
+                        int eachyearr = checkdatee.Year;
+                        if(eachyearr == 2015)
+                        {
+                            if (PRINCIPLE.ContainsKey(pba))
+                                PRINCIPLE[pba] += 1; //PRINCIPLE[key] = value. so, PRINCIPLE[pba]+=1 increments value2
+                            else  //First occurrence of string pba in 2015
+                                PRINCIPLE.Add(pba, 1);
+                        }
+                    }  //foreach     
+                }
+            }
+           
+          
+            table = new ConsoleTable("PRINCIPAL_BUSINESS_ACTIVITY_AS_PER_CIN ", "Count");
+            foreach (KeyValuePair<string, int> kvp in PRINCIPLE)
+            {
+                table.AddRow(kvp.Key, kvp.Value);
+            }
+            table.Write();
+
+            //TESTCASE 4
+
+           Dictionary<int, Dictionary<string, int>> GRUOPAGGR = new Dictionary<int, Dictionary<string, int>>();
+            //First adding 2000 to 2019  in dictionary to maintain the order of years in DICTIONARY
+            for (int k = 2000; k <= 2019; k++)
+            {
+                GRUOPAGGR.Add(k, new Dictionary<string, int>());
+            }
+            using (var reader = new StreamReader("C:\\Users\\ARCHANA ROSE BIJU\\Downloads\\Tripura.csv"))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+
+                    var rows = csv.GetRecords<CompanyMas>();
+                    foreach (var row in rows)
+                    {
+                        string pba = row.PRINCIPAL_BUSINESS_ACTIVITY_AS_PER_CIN;
+                        string dateStrr = row.DATE_OF_REGISTRATION;
+                        DateTime checkdatee = Convert.ToDateTime(dateStrr);
+                        int eachyearr = checkdatee.Year;
+                        if ((eachyearr >= 2000) && (eachyearr <= 2019))
+                        {
+                            if(!(GRUOPAGGR[eachyearr].ContainsKey(pba)))      // No single string in this year in dictionary , so add 1 , since FIRST OCCURRENCE of string
+                                GRUOPAGGR[eachyearr].Add(pba, 1);
+                            else if(GRUOPAGGR[eachyearr].ContainsKey(pba)) // string are already present so increment its count
+                                GRUOPAGGR[eachyearr][pba]+=1;
+                        }
+                    }
+                }
+            }
+            table = new ConsoleTable("PRINCIPAL_BUSINESS_ACTIVITY_AS_PER_CIN ", "Counts");
+            foreach (int key in GRUOPAGGR.Keys)
+            {
+                var value1 = GRUOPAGGR[key];
+                table.AddRow(key, "");
+                foreach (var key2 in value1.Keys)
+                {
+                    table.AddRow(key2, value1[key2]);
+                }
+            }
+                table.Write();
+                Console.ReadLine();
+         }
+    }
+}         
